bb33022aa87dd03e0bb153eacb8d6d1d3a0decf1
Lorne Booker <61119253+lornebookerr@users.noreply.github.com>
2026-02-07 22:57:02 +0000

    Refactor modal close button event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,8 +50,11 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+if (modalCloseBtn && overlay) {
+  modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+  overlay.addEventListener("click", testimonialsModalFunc);
+}
+
 
 
 
